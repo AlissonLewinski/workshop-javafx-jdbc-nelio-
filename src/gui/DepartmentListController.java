3e38ac51d01f66f44831a366a66f7b8a92221d44
Alisson <lucasali7@gmail.com>
2020-06-22 17:14:33 -0300

    DepartmentFormController

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -17,7 +17,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
